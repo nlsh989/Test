f632916c503936aed8e9d72df74c649979b07ba5
Nilesh Kumar <nlsh989@gmail.com>
2020-03-29 21:15:22 +0530

    Project1initiated
    Basic structure created. added Webservice manager and Ui controller.

---
+++ Unity2018.4 Projects/Project1/Assets/Scripts/WebserviceManagerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public delegate void ResponseDel(string res);
+
+public class WebserviceManagerScript : MonoBehaviour
+{
+    //url
+    private readonly string URL = "https://5e6b24f90f70dd001643c248.mockapi.io/v1/demo/math/data";
+
+    private string mJSONResponse = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetData(null);
+    }
+
+    public void GetData(ResponseDel callback)
+    {
+        if (mJSONResponse == null)
+        {
+            StartCoroutine(GetDataFromServer(callback));
+        }
+        else
+        {
+            callback?.Invoke(mJSONResponse);
+        }
+    }
+
+    private IEnumerator GetDataFromServer(ResponseDel callback)
+    {
+        //create post web request
+        UnityWebRequest webRequest = UnityWebRequest.Get(URL);
+        //calling and wait for response
+        yield return webRequest.SendWebRequest();
+        //check for if any error occurred
+        if (webRequest.isNetworkError || webRequest.isHttpError)
+        {
+            Debug.LogError(webRequest.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(webRequest.downloadHandler.text);
+            mJSONResponse = webRequest.downloadHandler.text;
+            callback?.Invoke(webRequest.downloadHandler.text);
+        }
+    }
+
+}
